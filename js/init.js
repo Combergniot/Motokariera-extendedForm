59c7b8151a36dd9cd5cc7cd33799b6ba270df797
Combergniot <marcin.wernerowicz@gmail.com>
2020-06-20 18:23:49 +0200

    initial commit

---
+++ js/init.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+    $('#availableFrom').datetimepicker({
+        useCurrent: false,
+        format: "HH:mm",
+        pickDate: false,
+        defaultTime: '01:00 PM'
+    });
+
+    $('#availableFrom').datetimepicker({
+        useCurrent: false,
+        format: "HH:mm",
+        pickDate: false,
+        defaultTime: '01:00 PM'
+    }
+
+    ) ;
+
+});
